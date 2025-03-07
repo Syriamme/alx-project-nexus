41527c57704b3f0ab239ede5e09f35943a646b7c
Syriamme <roridaniel01@gmail.com>
2025-03-07 10:07:49 +0300

    Fix requirements and update accounts URLs

--- ecommerce_backend/accounts/urls.py
+++ ecommerce_backend/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import register, login
+from django.http import JsonResponse
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('healthz/', health_check),
 ]
