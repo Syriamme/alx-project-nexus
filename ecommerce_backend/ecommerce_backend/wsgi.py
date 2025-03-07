b415be73eec6150e69770e0e0e857e15f804e0a7
Syriamme <roridaniel01@gmail.com>
2025-03-07 14:12:48 +0300

    Changes on file structure

--- ecommerce_backend/ecommerce_backend/wsgi.py
+++ ecommerce_backend/ecommerce_backend/wsgi.py
@@ -6,15 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-import sys
+
 import os
 
-# Add your project directory to the sys.path
-sys.path.append('/home/roridaniel/alx-project-nexus/ecommerce_backend/')
-
-# Set the settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ecommerce_backend.settings'
-
-# Get WSGI application
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+
 application = get_wsgi_application()
