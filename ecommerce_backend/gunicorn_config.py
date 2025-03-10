29a85aab0d75aef537b4d654376d01dafe7f7f1b
Syriamme <roridaniel01@gmail.com>
2025-03-11 02:18:59 +0300

    Added db_index

---
+++ ecommerce_backend/gunicorn_config.py
@@ -0,0 +1,6 @@
+import multiprocessing
+
+workers = 2 
+threads = 4 
+timeout = 120
+worker_class = "sync"
--- ecommerce_backend/products/models.py
+++ ecommerce_backend/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, db_index=True)  # Added db_index
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -10,9 +10,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)  # Added db_index
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)  # Added db_index
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
