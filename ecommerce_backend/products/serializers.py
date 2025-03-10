9e59bbe992117cda7e7227854956f96b40f4add1
Syriamme <roridaniel01@gmail.com>
2025-03-11 02:23:18 +0300

    updated serializers

--- ecommerce_backend/products/serializers.py
+++ ecommerce_backend/products/serializers.py
@@ -4,9 +4,10 @@ from .models import Category, Product
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
-
+        fields = ['id', 'name', 'description']
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'price', 'category', 'category_name', 'created_at', 'updated_at']
