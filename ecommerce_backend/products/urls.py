35fa9ea52109184a983e520087637cbfae66364e
Syriamme <roridaniel01@gmail.com>
2025-03-11 02:24:45 +0300

    updated urls

--- ecommerce_backend/products/urls.py
+++ ecommerce_backend/products/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import CategoryListCreateView, CategoryDetailView, ProductListCreateView, ProductDetailView
+from .views import (
+    CategoryListCreateView, CategoryDetailView,
+    ProductListCreateView, ProductDetailView, ProductListView
+)
 
 urlpatterns = [
-    # Categories
+    # Category URLs
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 
-    # Products
+    # Product URLs
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/list/', ProductListView.as_view(), name='product-list'),
 ]
