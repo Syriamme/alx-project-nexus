dc7156bdbff2c6e00230114c5c5d0f2964d74e40
Syriamme <roridaniel01@gmail.com>
2025-03-11 23:34:08 +0300

    cart is fully connected

--- ecommerce_backend/cart/urls.py
+++ ecommerce_backend/cart/urls.py
@@ -1,7 +1,10 @@
+
 from django.urls import path
-from .views import CartListView, CartDetailView
+from .views import CartListView, CartDetailView, AddToCartView, RemoveFromCartView
 
 urlpatterns = [
     path('cart/', CartListView.as_view(), name='cart-list'),
     path('cart/<int:pk>/', CartDetailView.as_view(), name='cart-detail'),
+    path('cart/add/', AddToCartView.as_view(), name='add-to-cart'),
+    path('cart/remove/', RemoveFromCartView.as_view(), name='remove-from-cart'),
 ]
--- ecommerce_backend/cart/views.py
+++ ecommerce_backend/cart/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
-from .models import Cart
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Cart, Product
 from .serializers import CartSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 class CartListView(generics.ListCreateAPIView):
     serializer_class = CartSerializer
@@ -16,3 +18,22 @@ class CartDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
+
+class AddToCartView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        product = get_object_or_404(Product, id=request.data.get('product_id'))
+        cart_item, created = Cart.objects.get_or_create(user=request.user, product=product)
+        cart_item.quantity += int(request.data.get('quantity', 1))
+        cart_item.save()
+        return Response({'message': 'Product added to cart successfully'}, status=status.HTTP_200_OK)
+
+class RemoveFromCartView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        product = get_object_or_404(Product, id=request.data.get('product_id'))
+        cart_item = get_object_or_404(Cart, user=request.user, product=product)
+        cart_item.delete()
+        return Response({'message': 'Product removed from cart successfully'}, status=status.HTTP_200_OK)
